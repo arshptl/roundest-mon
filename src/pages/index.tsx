ebf2bb3f33ccbc6fcfb5baec614df8433f39941a
arshptl <arshpatel714@gmail.com>
2022-08-05 20:00:31 +0530

    btn style added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import { useState } from "react";
 
+const btn =
+  "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 const Home: NextPage = () => {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -24,6 +26,12 @@ const Home: NextPage = () => {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
+
+  const voteForRoundest = (selected: number) => {
+    // TODO: fire mutation for persist changes
+
+    updateIds(getOptionsForVote());
+  }
   return (
     <>
       <Head>
@@ -38,20 +46,22 @@ const Home: NextPage = () => {
         <div className="">Which pokemon is Rounder?</div>
         <div className="p-2"></div>
         <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-          <div className="w-64 h-64 flex flex-col">
+          <div className="w-64 h-64 flex flex-col items-center">
             <img
               src={firstPokemon.data?.sprites.front_default}
               className="w-full"
             />
             <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+            <button className={btn} onClick={() => voteForRoundest(first as number)}>Roundest</button>
           </div>
           <div className="p-8">Vs</div>
-          <div className="w-64 h-64 flex flex-col">
+          <div className="w-64 h-64 flex flex-col items-center">
             <img
               src={secondPokemon.data?.sprites.front_default}
               className="w-full"
             />
             <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
+            <button className={btn} onClick={() => voteForRoundest(second as number)}>Roundest</button>
           </div>
           <div className="p-2"></div>
         </div>
